bc1370878d52b54e67ce0633d7a31a3497db41b1
cvVal <cvvaldez_00@hotmail.com>
2025-10-03 17:04:30 -0400

    Refactor player and portal prefabs; update UI main menu to dynamically add a `Continue` btn based on saved data

--- Assets/Scripts/UI/UIMainMenuState.cs
+++ Assets/Scripts/UI/UIMainMenuState.cs
@@ -1,31 +1,58 @@
 using RPG.Core;
+using RPG.Utility;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace RPG.UI
 {
     public class UIMainMenuState : UIBaseState
     {
+        private int _sceneIndex;
+
         public UIMainMenuState(UIController ui) : base(ui)
         {
         }
 
         public override void EnterState()
         {
-            UIController.MainMenuContainer.style.display = DisplayStyle.Flex;
-            
-            UIController.Buttons = UIController.MainMenuContainer.Query<Button>(null, "menu-button").ToList();
+            if (PlayerPrefs.HasKey(Constants.PlayerPrefsSceneIndex))
+            {
+                _sceneIndex = PlayerPrefs.GetInt(Constants.PlayerPrefsSceneIndex);
+                AddButton();
+            }
 
-            UIController.Buttons[0].AddToClassList("active");
+            UIController.MainMenuContainer.style.display = DisplayStyle.Flex;
+
+            UIController.Buttons = UIController.MainMenuContainer
+                .Query<Button>(null, Constants.UIClassMenuButton)
+                .ToList();
+
+            UIController.Buttons[0].AddToClassList(Constants.UIClassActive);
         }
 
         public override void SelectButton()
         {
             var btn = UIController.Buttons[UIController.currentSelection];
 
-            if (btn.name == "start-button")
+            if (btn.name == Constants.UIClassStartButton)
             {
+                PlayerPrefs.DeleteAll();
                 SceneTransition.Initiate(1);
             }
+            else
+            {
+                SceneTransition.Initiate(_sceneIndex);
+            }
+        }
+
+        private void AddButton()
+        {
+            var continueButton = new Button();
+            continueButton.AddToClassList(Constants.UIClassMenuButton);
+            continueButton.text = "Continue";
+
+            var mainMenuButtons = UIController.MainMenuContainer.Q<VisualElement>(Constants.UIClassButtons);
+            mainMenuButtons.Add(continueButton);
         }
     }
 }
